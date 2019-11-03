43b8acd01470d3bf086520697fbae4d77b9e56b7
captain-redbeard <troy.ross@hosvir.com>
2019-11-03 19:24:53 +1000

    Updates

---
+++ precache-manifest.6203ae95c400f8bbd2ea36f6758ba063.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f7e58b98fc2310d33f2e0850f5e3ce",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "5d385b9828df92526c4c",
+    "url": "/todo-app-react/static/css/2.c3b54786.chunk.css"
+  },
+  {
+    "revision": "2031fba6d0cdc0ddc94d",
+    "url": "/todo-app-react/static/css/main.c4089acd.chunk.css"
+  },
+  {
+    "revision": "5d385b9828df92526c4c",
+    "url": "/todo-app-react/static/js/2.68e1956e.chunk.js"
+  },
+  {
+    "revision": "2031fba6d0cdc0ddc94d",
+    "url": "/todo-app-react/static/js/main.9963bbff.chunk.js"
+  },
+  {
+    "revision": "940942c8f9da54e12bc9",
+    "url": "/todo-app-react/static/js/runtime-main.70eba0c8.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/todo-app-react/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/todo-app-react/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/todo-app-react/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/todo-app-react/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/captain-redbeard/todo-app-react/precache-manifest.2cdbdd1e5baecb58714ec8541b07cb69.js"
+  "/todo-app-react/precache-manifest.6203ae95c400f8bbd2ea36f6758ba063.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/captain-redbeard/todo-app-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-app-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
